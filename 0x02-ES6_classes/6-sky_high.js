ca680d4696d064499c62791bad62ecee8adb20f0
010binary <augustine32fg@gmail.com>
2024-06-28 01:58:59 +0000

    correct 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
@@ -13,7 +13,8 @@ export default class SkyHighBuilding extends Building {
   set floors(value) {
     this._floors = value;
   }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
