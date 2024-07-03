e2bc909979b54b6fb0fda586a6cd5fdc971708cf
010binary <augustine32fg@gmail.com>
2024-07-03 09:32:31 +0000

    Using Reduce function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,9 @@
-export default function getStudentIdsSum(list) {
-    if (list instanceof Array){
-        return list.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);   
-    }
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
 }
