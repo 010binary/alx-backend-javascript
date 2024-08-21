2493833db32d01fbcf9e4a5dcdbf38a4f22220f9
010binary <augustine32fg@gmail.com>
2024-08-21 00:46:12 +0000

    writing

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+/**
+ * reading and writing from the stdin and stdout.
+ * @param {String} content from reading.
+ */
+PROCESS.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+PROCESS.stdin.
