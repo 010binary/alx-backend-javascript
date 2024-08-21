b2b6c6be59cc0494cc2d8fae6c3e5e227a944aed
010binary <augustine32fg@gmail.com>
2024-08-21 04:41:16 +0000

    linting

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,10 +1,10 @@
-import express from "express";
-import router from "./routes/index";
+import express from 'express';
+import router from './routes/index';
 
 const app = express();
 const PORT = 1234;
 
-app.use("/", router);
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
