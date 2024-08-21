272039741f77565e77a5792bfd30d3f659f8b754
010binary <augustine32fg@gmail.com>
2024-08-21 02:15:45 +0100

    linting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@
  * reading and writing from the stdin and stdout.
  * @param {String} content from reading.
  */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("readable", () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name) {
@@ -12,6 +12,6 @@ process.stdin.on("readable", () => {
   }
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
