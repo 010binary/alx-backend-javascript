0f2b7a65c129ec0f7d3ab5b1f27047a680d6ef45
010binary <augustine32fg@gmail.com>
2024-08-22 12:07:17 +0000

    fixed the linting issue

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import StudentsController from '../controllers/StudentsController.js';
-import AppController from '../controllers/AppController.js';
+import StudentsController from '../controllers/StudentsController';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
